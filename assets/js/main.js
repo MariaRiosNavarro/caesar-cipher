4b47df43b974f167b3309252264120ad6d945b71
SuperCoder <propscode254@gmail.com>
2023-09-28 12:26:54 +0200

    add first 2 arrays

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,27 +12,85 @@ const plaintext = [
   "c",
   "d",
   "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
+  //   "f",
+  //   "g",
+  //   "h",
+  //   "i",
+  //   "j",
+  //   "k",
+  //   "l",
+  //   "m",
+  //   "n",
+  //   "o",
+  //   "p",
+  //   "q",
+  //   "r",
+  //   "s",
+  //   "t",
+  //   "u",
+  //   "v",
+  //   "w",
+  //   "x",
+  //   "y",
+  //   "z",
 ];
 
-console.log(plaintext.length);
+console.log(plaintext.length); //26 -> last Index 25
+
+const lastIndexArray = plaintext.length - 1; //25
+
+const decoder = () => {
+  // 3.Save values
+
+  let text = textInput.value;
+  let key = Number(keyInput.value);
+  let index = "";
+
+  console.log(text, key);
+
+  //   4. Save a new array Index Array
+
+  let indexArray = [];
+
+  indexArray = plaintext.map((letter) => plaintext.indexOf(letter));
+
+  console.log("indexArray", indexArray);
+
+  //   5. Add the index + value a new array Index, with the limit of 25n (lastIndexArray)
+
+  let newIndex = 0;
+
+  let indexArrayWithKey = [];
+
+  indexArrayWithKey = indexArray.map((index) => {
+    for (let i = 0; i < lastIndexArray; i++) {
+      let numberIndex = Number(index);
+      newIndex = numberIndex + key;
+      //   if we are at the end of the array (25), we need to count at 0
+      if (newIndex >= lastIndexArray) {
+        let newValue = numberIndex + key - lastIndexArray;
+        newIndex = newValue;
+      }
+      return newIndex;
+    }
+  });
+
+  console.log("withKey", indexArrayWithKey);
+
+  //   6. Use the new index to make a new output array
+
+  // Array.from()
+};
+
+// ---
+
+// test
+
+// const encoder = () => {
+//   // 3.Save values
+
+//   let text = textInput.value;
+//   let key = keyInput.value;
+
+//   console.log(text, key);
+// };
