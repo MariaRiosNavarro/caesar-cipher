9262d4492a50da3047fd5ca97c93449f3722ca84
SuperCoder <propscode254@gmail.com>
2023-09-28 10:41:28 +0200

    change html

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+// 1. Save variables
+
+const textInput = document.querySelector('[data-js="text"]');
+const keyInput = document.querySelector('[data-js="key"]');
+const output = document.querySelector('[data-js="output"]');
+
+// 2. Save Alphabet
+
+const plaintext = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+console.log(plaintext.length);
